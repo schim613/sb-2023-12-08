3ba48f4a2ddb8b06abe405c92a06f1ab47874884
schim613 <uximin@naver.com>
2023-12-08 15:36:42 +0900

    DB connect, join

---
+++ src/main/java/com/ll/sb20231208/domain/member/member/entity/Member.java
@@ -0,0 +1,25 @@
+package com.ll.sb20231208.domain.member.member.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
+@Builder
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/ll/sb20231208/global/rsData/RsData.java
@@ -0,0 +1,25 @@
+package com.ll.sb20231208.global.rsData;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@AllArgsConstructor(access = PROTECTED)
+@Getter
+public class RsData<T> {
+    private final String resultCode;
+    private final String msg;
+    private final T data;
+    private final int statusCode;
+
+    public static <T> RsData<T> of(String resultCode, String msg, T data) {
+        int statusCode = Integer.parseInt(resultCode);
+
+        return new RsData<>(resultCode, msg, data, statusCode);
+    }
+
+    public boolean isSuccess() {
+        return statusCode >= 200 && statusCode < 400;
+    }
+}
