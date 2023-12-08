31c15ea8f99a4ad748e634d3afb40154ba2522ca
schim613 <uximin@naver.com>
2023-12-08 10:29:26 +0900

    setting

---
+++ src/main/java/com/ll/sb20231208/Sb20231208Application.java
@@ -0,0 +1,13 @@
+package com.ll.sb20231208;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb20231208Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb20231208Application.class, args);
+	}
+
+}
