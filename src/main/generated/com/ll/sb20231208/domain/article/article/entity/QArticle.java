6f525a180074260e856866c656d6df32d069358f
schim613 <uximin@naver.com>
2023-12-11 13:48:51 +0900

    article write test

---
+++ src/main/generated/com/ll/sb20231208/domain/article/article/entity/QArticle.java
@@ -0,0 +1,41 @@
+package com.ll.sb20231208.domain.article.article.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QArticle is a Querydsl query type for Article
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QArticle extends EntityPathBase<Article> {
+
+    private static final long serialVersionUID = -280071441L;
+
+    public static final QArticle article = new QArticle("article");
+
+    public final StringPath body = createString("body");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath title = createString("title");
+
+    public QArticle(String variable) {
+        super(Article.class, forVariable(variable));
+    }
+
+    public QArticle(Path<? extends Article> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QArticle(PathMetadata metadata) {
+        super(Article.class, metadata);
+    }
+
+}
+
---
+++ src/test/java/com/ll/sb20231208/domain/member/member/service/MemberServiceTest.java
@@ -0,0 +1,28 @@
+package com.ll.sb20231208.domain.member.member.service;
+
+import com.ll.sb20231208.domain.member.member.entity.Member;
+import com.ll.sb20231208.global.rsData.RsData;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+public class MemberServiceTest {
+    @Autowired
+    private MemberService memberService;
+
+    @DisplayName("회원가입")
+    @Test
+    void t1() {
+        RsData<Member> joinRs = memberService.join("usernew", "1234");
+        Member member = joinRs.getData();
+
+        assertThat(member.getId()).isGreaterThan(0L);
+    }
+}
