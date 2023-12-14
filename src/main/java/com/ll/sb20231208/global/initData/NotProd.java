356fefcb4d850252549d0d2406dfd249c5b94e7b
schim613 <uximin@naver.com>
2023-12-14 15:22:27 +0900

    step 3

--- src/main/java/com/ll/sb20231208/global/initData/NotProd.java
+++ src/main/java/com/ll/sb20231208/global/initData/NotProd.java
@@ -63,6 +63,11 @@ public class NotProd {
         article2.addComment(member1, "댓글4");
         article2.addComment(member1, "댓글5");
 
+        article1.addTag("자바");
+        article1.addTag("백엔드");
+        article1.addTag("프레임워크");
+        article1.addTag("스프링부트");
+
         // throw new RuntimeException("강제로 예외를 발생시킵니다.");
         // 트랜젝션 중에 런타임예외가 발생하면 전체가 없던 일이 되어버림
     }
