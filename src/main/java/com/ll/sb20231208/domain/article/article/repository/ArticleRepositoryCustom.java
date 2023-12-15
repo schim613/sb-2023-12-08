38623ed61375497752b3b3a5299731792dec2f18
schim613 <uximin@naver.com>
2023-12-16 02:09:10 +0900

    step 15

--- src/main/java/com/ll/sb20231208/domain/article/article/repository/ArticleRepositoryCustom.java
+++ src/main/java/com/ll/sb20231208/domain/article/article/repository/ArticleRepositoryCustom.java
@@ -1,4 +1,11 @@
 package com.ll.sb20231208.domain.article.article.repository;
 
+import com.ll.sb20231208.domain.article.article.entity.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface ArticleRepositoryCustom {
+    Page<Article> search(List<String> kwTypes, String kw, Pageable pageable);
 }
